55ed40b1b31b01b3e60eba88ca17b7fb312d205d
lltchen <lltchen1@gmail.com>
2018-11-20 21:24:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ var katzDeliLine = [];
 var line = []
 
 function takeANumber(katzDeli, name) {
-  katzDeliLine.push(`${name}`);
-  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+    katzDeliLine.push(`${name}`);
+    return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
 
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length == 0){
-    return("There is nobody waiting to be served!")
-  }
-else (){
-  return(`Currently serving ${katzDeliLine[0].shift()}`)
-  }
+    if (katzDeliLine.length == 0){
+      return("There is nobody waiting to be served!")
+    }
+  else (){
+    return(`Currently serving ${katzDeliLine[0].shift()}`)
+    }
 }
+
+
 function currentLine(katzDeliLine){
-  return(`The line is currently: 1.${katzDeliLine[0]}, 2.${katzDeliLine[1]}, .${katzDeliLine[3]}`)
+  var line = []
+  for (var i = 0; i < katzDeliLine.length; i++) {
+  line.push(`${i}.` +katzDeliLine[i])
+  }
+      return("The line is currently:" + line )
 
 }
