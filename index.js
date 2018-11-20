7b039893665702832c49775769adb5be7715c1f1
lltchen <lltchen1@gmail.com>
2018-11-20 21:27:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function nowServing(katzDeliLine){
 function currentLine(katzDeliLine){
   var line = []
   for (var i = 0; i < katzDeliLine.length; i++) {
-  line.push(`${i}.` +katzDeliLine[i])
+  line.push(`${i}+1.` +katzDeliLine[i])
   }
       return("The line is currently:" + line )
 
