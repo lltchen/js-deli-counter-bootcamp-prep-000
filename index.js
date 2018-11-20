25095f33740a4323645bdca5a43dbe9370542938
lltchen <lltchen1@gmail.com>
2018-11-20 20:24:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeliLine = []
+var getOnLine = katzDeliLine.push(getOnLine)
 var numberOnLine = katzDeliLine.length;
 
 function takeANumber(katzDeliLine,name){
-
-  console.log(`Welcome, ${name}. You are number ${numberOnLine} in line.`)
-}
+    console.log(`Welcome, ${name}. You are number ${numberOnLine} in line.`)
+    }
